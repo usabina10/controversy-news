51ff3223f454346beee7a5bc32432118b70274ee
usabina10 <87382391+usabina10@users.noreply.github.com>
2026-02-11 17:49:15 +0200

    Update next.config.ts

--- news-debate-app/next.config.ts
+++ news-debate-app/next.config.ts
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        '@': './src',
-        '@/': './src/'
-      }
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    resolveAlias: {
+      '@': './src',
+      '@/': './src/'
     }
   }
 };
-module.exports = nextConfig;
+
+export default nextConfig;
